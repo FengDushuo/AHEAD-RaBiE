165b698dd156792034d5bcab3f1dce95ec12ddbd
Doshow Feng <39474576+FengDushuo@users.noreply.github.com>
2025-12-28 21:43:28 +0800

    Update model path in finetune script

--- static/data/llm-app-Llama-31-8B-finetune.sh
+++ static/data/llm-app-Llama-31-8B-finetune.sh
@@ -1,6 +1,6 @@
 # sh examples/custom/infer.sh
 CUDA_VISIBLE_DEVICES=1 \
 swift app \
-    --model llm-output/Llama-3.1-8B-ALP-finetune/v0-20250718-001606/checkpoint-10000-merged \
+    --model checkpoint-10000-merged \
     --stream true \
     --server_port 7860
